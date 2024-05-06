e753f3070f270124195fcc872f62ec6d3e077bc7
levannghia <levannghia.tdc2018@gmail.com>
2024-05-06 15:13:49 +0700

    Update

--- app/Http/Controllers/Api/V1/NotificationController.php
+++ app/Http/Controllers/Api/V1/NotificationController.php
@@ -13,16 +13,11 @@ class NotificationController extends Controller
 
     private function getAccessToken()
     {
-
-        // Đường dẫn đến tệp JSON chứa Service Account Credentials
         // $serviceAccountFile = storage_path('app/eventhub-4c23c.json');
-
         // $key = json_decode(file_get_contents(storage_path('app/eventhub-4c23c.json')), true);
+        // 'registration_ids' => $request->input('fcmTokens'),
         // composer remove google/auth
-
-        // $serviceAccountCredentials = json_decode(file_get_contents(storage_path('app/eventhub-4c23c.json')), true);
-
-        $credentialsFilePath = storage_path('app/eventhub-4c23c.json'); //replace this with your actual path and file name
+        $credentialsFilePath = storage_path('app/eventhub-4c23c.json');
         $client = new \Google_Client();
         $client->setAuthConfig($credentialsFilePath);
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
@@ -36,8 +31,7 @@ class NotificationController extends Controller
         $validator = Validator::make($request->all(), [
             "title" => ["string", "nullable"],
             "body" => ["string", "nullable"],
-            "subtitle" => ["string", "nullable"],
-            "fcmTokens" => ["required", "string"],
+            "fcmToken" => ["required", "string"],
         ]);
 
         if ($validator->fails()) {
@@ -49,24 +43,29 @@ class NotificationController extends Controller
 
         $token = 'AAAA29q30NE:APA91bHxrWGXNWsrtVVu6eVit85U7a62frJaiKO7og_TYdBWWmyUTHKq-X1zIC7A7aKbyJ1a_opPU2Hsj3K-wau5XZdepKW7jcdBKBRexJeuUPLOj1m6D7obcSH7YThL5l8vry0zsZ5s';
         $accessToken = $this->getAccessToken();
-
+      
         try {
             $response = Http::withToken($accessToken)->acceptJson()->post('https://fcm.googleapis.com/v1/projects/eventhub-4c23c/messages:send', [
                 "message" => [
-                    // 'registration_ids' => $request->input('fcmTokens'),
-                    "token" => $request->input('fcmTokens'),
+                    "token" => $request->input('fcmToken'),
                     "notification" => [
-                        // "topic" => "news",
                         "title" => $request->input('title') ?? '',
                         "body" => $request->input('body') ?? '',
-                        "subtitle" => $request->input('subtitle') ?? '',
-                        // "sound" => "default"
                     ],
                     "data" => [
-                        'id' => 12
+                        'id' => "12"
                     ]
                 ]
             ]);
+
+            if ($response->failed()) {
+                $error = $response->json('error');
+                Log::error("Lỗi FCM: " . $error['message']);
+                return response()->json([
+                    'error' => $error['message'],
+                    'message' => 'Gửi thông báo thất bại!',
+                ], 500);
+            }
 
             return response()->json([
                 'message' => 'Send notification success!',
